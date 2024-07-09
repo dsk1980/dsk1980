173257bde7bac794260a97a062327b1d69955d33
dsk1980 <dsk1980@yandex.ru>
2024-07-09 15:42:15 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+print('Практическое задание по уроку "Строки и индексация строк"')
+example = 'Топинамбур' # Задача
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
