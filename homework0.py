ae076d11457482c35fb9e2b5ffba3102764f1596
dsk1980 <dsk1980@yandex.ru>
2024-07-09 15:40:40 +0300

    Add files via upload

---
+++ homework0.py
@@ -0,0 +1,6 @@
+print('Практическое задание по уроку "Базовые структуры данных"')
+print(12+89-1) # Предисловие
+print("1st program", 9**0.5*5) # 1st program
+print("2st program", 9.99>9.98 and 1000!=1000.1) # 2st program
+print("3st program", 1234%1000//10+5678%1000//10) # 3st program
+print("4st program", int(13.42)==42.13*100%100 or 42.13*100//100==int(42.13)) # 4st program
