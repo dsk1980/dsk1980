b8ad4286e30d7108817abd4120f8723ffe3b6d45
dsk1980 <dsk1980@yandex.ru>
2024-07-12 13:56:49 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,6 +2,7 @@ print('Практическое задание по теме "Переменны
 Number_of_completed_homework_assignments=12
 Number_of_hours_spent=1.5
 Course_name='Python'
-print('a-kоличество выполненных ДЗ-',Number_of_completed_homework_assignments,',','b-kоличество затраченных часов-',Number_of_hours_spent,',','c-время на одно задание-',Number_of_hours_spent/Number_of_completed_homework_assignments)
+Time_for_one_task=Number_of_hours_spent/Number_of_completed_homework_assignments
+print('a-kоличество выполненных ДЗ-',Number_of_completed_homework_assignments,',','b-kоличество затраченных часов-',Number_of_hours_spent,',','c-время на одно задание-',Time_for_one_task)
 Time_for_one_task=0.125
 print('Курс:',Course_name,',','всего задач:', Number_of_completed_homework_assignments,',','затрачено часов:',Number_of_hours_spent,',','среднее время выполнения:',Time_for_one_task,'часа.')
