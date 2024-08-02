9831f7959d5e2c4e9de10673c1a2bf6e89b11c5b
dsk1980 <dsk1980@yandex.ru>
2024-08-02 07:14:00 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,8 +1,8 @@
-name=input('Введите ваше имя: ')
-print('Здравствуйте,',name)
-number1=int(input('Введите первое число: '))
-number2=int(input('Введите второе число: '))
-number3=int(input('Введите третье число: '))
+name = input('Введите ваше имя: ')
+print('Здравствуйте,', name)
+number1 = int(input('Введите первое число: '))
+number2 = int(input('Введите второе число: '))
+number3 = int(input('Введите третье число: '))
 if number1 == number2 and number2 == number3 and number3 == number1:
     print('3')
 elif number1 == number2 != number3 or number2 == number3 != number1 or number3 == number1 != number2:
