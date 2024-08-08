96c5d2180c51ef1102033679694ca24dc7933838
dsk1980 <dsk1980@yandex.ru>
2024-08-08 18:45:22 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,6 +1,3 @@
-name = input('Введите ваше имя: ')
-print('Здравствуйте,', name)
-print('Домашняя работа по уроку "Функции в Python.Функция с параметром" - Задача "Матрица воплоти"')
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
@@ -8,9 +5,6 @@ def get_matrix(n, m, value):
         for j in range(m):
             matrix[i].append(value)
         print(matrix)
-    return matrix
-n = int(input('Введите количество строк матрицы: '))
-m = int(input('Введите количество столбцов матрицы: '))
-value = input(f'Задайте значения матрицы : ')
-print('' * m)
-matrix = get_matrix(n, m, value)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
