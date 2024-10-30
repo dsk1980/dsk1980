2d40bb7cf58ce724d239330d62c081f8f47ed991
dsk1980 <dsk1980@yandex.ru>
2024-10-30 11:44:37 +0300

    Add files via upload

---
+++ module_6_3.py
@@ -0,0 +1,41 @@
+class Horse:
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
+
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, by):
+        self.y_distance += by
+
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        pos_pegasus = (self.x_distance, self.y_distance)
+        return pos_pegasus
+
+    def voice(self):
+        print(self.sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
