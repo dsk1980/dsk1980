c762a459d3a9d13582acca79fe35793bcd095399
dsk1980 <dsk1980@yandex.ru>
2024-11-14 10:44:33 +0300

    Add files via upload

---
+++ m_1_1.py
@@ -0,0 +1,21 @@
+# Базовые структуры данных
+
+print("1st program")
+x = 9**0.5*5
+print(x)
+print()
+
+print("2st program")
+y = 9.99>9.98 and 1000!=1000.1
+print(y)
+print()
+
+print("3st program")
+a = 2*2+2
+b = 2*(2+2)
+c = a==b
+print(a, b, c)
+print()
+
+print("4st program")
+print(int(float('123.456')*10)%10)
