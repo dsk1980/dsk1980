ea312826aadb64d41d0c23a8da3cdb0ca7934256
dsk1980 <dsk1980@yandex.ru>
2025-01-13 09:35:53 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,11 +1,14 @@
 # Домашнее задание по теме "Генераторы"
 
 def all_variants(text):
+    n = len(text)
+    for length in range(1, n + 1):
+        for start in range(n - length + 1):
+            substring = text[start:start + length]
+            yield substring
 
-    for i in range(len(text)):
-        for j in range(i, len(text)):
-            yield text[i:j+1]
 
 a = all_variants("abc")
 for s in a:
     print(s)
+print()
